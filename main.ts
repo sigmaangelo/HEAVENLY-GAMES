5c3ae2503c3f191305d427fde0bd90c0df324b8d
sigmaangelo <angelo.frank.george@gmail.com>
2025-11-30 21:56:48 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 
-const PASSWORD = "1234";
+const PASSWORD = "ᎤᏇᏕ1Ꮭ0ᎧᏰᎧᎥᏬᎩᏖᏒᎴ";
 
 serve(async (req) => {
   const url = new URL(req.url);
